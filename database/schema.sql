85a3d3082f23e15153f8efb49a893cc4ba5def6d
graheed <graheed@gmail.com>
2023-12-02 20:57:53 -0500

    Finished the database setup, it can now be accessed from any file in project

---
+++ database/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    role VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS Contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telephone VARCHAR(255) NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    assigned_to INT NOT NULL,
+    created_by INT NOT NULL,
+    created_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME
+);
+
+CREATE TABLE IF NOT EXISTS Notes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    contact_id INT NOT NULL,
+    comment TEXT,
+    created_by INT NOT NULL,
+    created_at DATETIME DEFAULT NOW()
+);
+
+INSERT INTO Users (firstname, lastname, password, email, role)
+VALUES ("Nikola", "Tesla", "password123", "admin@project2.com", "Admin");
+
